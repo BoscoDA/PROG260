2603446855d0883333424eadaa8f85a181e4f023
BoscoDA <nickwe02@gmail.com>
2023-09-08 13:34:53 -0500

    add logic to determine and out the most frequent genre

--- Week 1/Fundamentals/Program.cs
+++ Week 1/Fundamentals/Program.cs
@@ -22,7 +22,21 @@ namespace Fundamentals
             Console.WriteLine($"Total games: {Games.Count}\n");
 
             //Display most frequent genre
+            Dictionary<string, int> genres = new Dictionary<string, int>();
+            foreach (var game in gameInfo.MetaData)
+            {
+                if (genres.ContainsKey(game.Genre))
+                {
+                    genres[game.Genre]++;
+                }
+                else
+                {
+                    genres.Add(game.Genre, 0);
+                }
+            }
 
+            var MostFreqGenre = genres.OrderByDescending(x => x.Value).ToList();
+            Console.WriteLine($"The most frequent genre is: {MostFreqGenre[0].Key}\n");
 
             //Display which map names have the most number of characters excluding spaces and which game they belong to
 
