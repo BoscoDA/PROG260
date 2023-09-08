dbb64c0040c3db3ad3142813f8e8d90d6f409b72
BoscoDA <nickwe02@gmail.com>
2023-09-08 13:32:41 -0500

    Initial commit

---
+++ Week 1/Fundamentals/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameInfo gameInfo = new GameInfo();
+            Dictionary<int, Info> Games = new Dictionary<int, Info>();
+
+            foreach (Info info in gameInfo.MetaData)
+            {
+                Games.Add(info.Id, info);
+            }
+
+            //Display number of games
+            
+
+            //Display most frequent genre
+            
+
+            //Display which map names have the most number of characters excluding spaces and which game they belong to
+
+
+            //Display all info as a dictionary
+            
+            //Display map names with letter z in them
+            
+
+        }
+    }
+}
