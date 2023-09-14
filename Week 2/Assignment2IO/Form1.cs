379d81d5d86af77d1831d0a7fc70e80361264d52
BoscoDA <nickwe02@gmail.com>
2023-09-14 18:28:29 -0500

    implement character count method

--- Week 2/Assignment2IO/Form1.cs
+++ Week 2/Assignment2IO/Form1.cs
@@ -29,9 +29,41 @@ namespace Assignment2IO
             return output;
         }
 
+        private Dictionary<char, int> GetCharacterCounts(string filePath)
+        {
+            Dictionary<char, int> charCounts = new Dictionary<char, int>();
+
+            StreamReader reader = new StreamReader(filePath);
+            string file = reader.ReadToEnd();
+
+            file = Utilities.RemoveWhiteSpace(file);
+
+            file = Utilities.RemoveEscapeChars(file);
+
+            foreach (char c in file)
+            {
+                if ((charCounts.ContainsKey(c) == true) && (c != ' '))
+                {
+                    charCounts[c]++;
+                }
+                else
+                {
+                    if(c != ' ')
+                    {
+                        charCounts.Add(c, 1);
+                    }
+                    
+                }
+            }
+            reader.Close();
+
+            return charCounts;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(ReadFileName(textBox1.Text));
+            listBox1.Items.Add($"{ReadFileName(textBox1.Text)}{Environment.NewLine}");
+            GetCharacterCounts(textBox1.Text).ToList().ForEach(x => listBox1.Items.Add($"{x.Key}: {x.Value}"));
         }
     }
 }
