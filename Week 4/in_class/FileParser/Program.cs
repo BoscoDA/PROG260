18058271abf0c2788347306b048fb24bf71c2a53
BoscoDA <382963@colum.edu>
2023-09-28 17:12:46 -0500

    completed in class assignment

--- Week 4/in_class/FileParser/Program.cs
+++ Week 4/in_class/FileParser/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var path = Directory.GetCurrentDirectory() + @"\Files\Demo.txt";
+
+            using(StreamReader sr = new StreamReader(path)) 
+            {
+                var line = sr.ReadLine();
+
+                while(line != null)
+                {
+                    var stat = line.Split('|');
+
+                    foreach(var item in stat)
+                    {
+                        Console.Write(item + " ");
+                    }
+
+                    line = sr.ReadLine();
+                }
+            }
         }
     }
 }
