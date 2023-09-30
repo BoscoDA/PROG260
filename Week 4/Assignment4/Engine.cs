9d92e7844b28ed43ed23cf038684c5afb467a7ca
BoscoDA <nickwe02@gmail.com>
2023-09-30 10:04:57 -0500

    Refactor to apply OOP principles and output to files and not console

---
+++ Week 4/Assignment4/Engine.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    internal class Engine
+    {
+        public void ProcessFiles(List<IDelimiterFile> filesToProcess)
+        {
+            try
+            {
+                foreach (var file in filesToProcess)
+                {
+                    string outputFilePath = file.Path.Replace(file.FileExtension, $"_out.txt");
+
+                    if (File.Exists(outputFilePath))
+                    {
+                        File.Delete(outputFilePath);
+                    }
+
+                    using (StreamReader sr = new StreamReader(file.Path))
+                    {
+                        using (StreamWriter sw = new StreamWriter(outputFilePath))
+                        {
+                            var line = sr.ReadLine();
+                            var lineCount = 0;
+                            while (line != null)
+                            {
+                                lineCount++;
+                                sw.Write($"Line #{lineCount} :");
+                                var fields = line.Split(file.Delimiter);
+                                var fieldCount = 0;
+                                foreach (var field in fields)
+                                {
+                                    fieldCount++;
+                                    sw.Write($"Field #{fieldCount} = {field}");
+                                    if (fieldCount - 1 < fields.Count() - 1)
+                                    {
+
+                                        sw.Write(" ==> ");
+                                    }
+                                    else
+                                    {
+                                        sw.Write($"{Environment.NewLine}");
+                                    }
+                                }
+
+                                sw.Write($"{Environment.NewLine}");
+                                line = sr.ReadLine();
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (IOException exception)
+            {
+
+            }
+            catch(Exception exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ Week 4/Assignment4/Parser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment4
+{
+    internal class Parser
+    {
+        List<IDelimiterFile> filesToProcess { get; set; }
+        List<Error> errors { get; set; }
+        Engine engine { get; set; }
+
+        public Parser()
+        {
+            filesToProcess = new List<IDelimiterFile>();
+            errors = new List<Error>();
+            engine = new Engine();
+        }
+
+        public void ParseFiles(string directoryPath)
+        {
+            List<string> files = GetFilesToProcess(directoryPath);
+            GenerateFiles(files);
+            engine.ProcessFiles(filesToProcess);
+        }
+
+        private List<string> GetFilesToProcess(string directoryPath)
+        {
+            var files = Directory.GetFiles(directoryPath).Where(x => !x.Contains("_out")).ToList();
+            return files;
+        }
+
+        private void GenerateFiles(List<string> files)
+        {
+            foreach (var file in files)
+            {
+                if (file.EndsWith(".txt"))
+                {
+                    MyFile tempFile = new MyFile(file, '|', ".txt");
+                    filesToProcess.Add(tempFile);
+                }
+                else if (file.EndsWith(".csv"))
+                {
+                    MyFile tempFile = new MyFile(file, ',', ".csv");
+                    filesToProcess.Add(tempFile);
+                }
+            }
+        }
+    }
+}
