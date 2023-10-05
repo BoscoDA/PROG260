30d3157259525f222c17c682d4fb463f3a5b99cc
Nicholas Eisner <nickwe02@gmail.com>
2023-10-05 17:35:20 -0500

    implement assignment 4 as base for this assignment

---
+++ Week 5/Assignment5/Engines/BaseEngine.cs
@@ -0,0 +1,79 @@
+﻿using Assignment5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5.Engines
+{
+    internal class Engine
+    {
+
+        /// <summary>
+        /// Takes in a list of type IDelimiterFile and sequentially processes them to a output file 
+        /// </summary>
+        /// <param name="filesToProcess">List of IDelimiterFile objects to be processed</param>
+        /// <returns></returns>
+        public List<Error> ProcessFiles(List<IFile> filesToProcess)
+        {
+            List<Error> errors = new List<Error>();
+            try
+            {
+                foreach (var file in filesToProcess)
+                {
+                    string outputFilePath = file.Path.Replace(file.FileExtension, $"_out{Constants.FileExtensions.TEXT}");
+
+                    if (File.Exists(outputFilePath))
+                    {
+                        File.Delete(outputFilePath);
+                    }
+
+                    using (StreamReader sr = new StreamReader(file.Path))
+                    {
+                        using (StreamWriter sw = new StreamWriter(outputFilePath))
+                        {
+                            var line = sr.ReadLine();
+                            var lineCount = 0;
+                            while (line != null)
+                            {
+                                lineCount++;
+                                sw.Write($"Line #{lineCount} :");
+                                var fields = line.Split(file.Delimiter);
+                                var fieldCount = 0;
+                                foreach (var field in fields)
+                                {
+                                    fieldCount++;
+                                    sw.Write($"Field #{fieldCount} = {field}");
+                                    if (fieldCount - 1 < fields.Count() - 1)
+                                    {
+
+                                        sw.Write(" ==> ");
+                                    }
+                                    else
+                                    {
+                                        sw.Write($"{Environment.NewLine}");
+                                    }
+                                }
+
+                                sw.Write($"{Environment.NewLine}");
+                                line = sr.ReadLine();
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (IOException exception)
+            {
+                errors.Add(new Error(exception.Message, "Engine.ProcessFiles()"));
+            }
+            catch (Exception exception)
+            {
+                errors.Add(new Error(exception.Message, "Engine.ProcessFiles()"));
+            }
+
+            return errors;
+        }
+    }
+}
---
+++ Week 5/Assignment5/Parser.cs
@@ -0,0 +1,118 @@
+﻿using Assignment5.Engines;
+using Assignment5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    internal class Parser
+    {
+        List<IFile> filesToProcess { get; set; }
+        List<Error> errors { get; set; }
+        Engine engine { get; set; }
+
+        public Parser()
+        {
+            filesToProcess = new List<IFile>();
+            errors = new List<Error>();
+            engine = new Engine();
+        }
+
+        /// <summary>
+        /// Process all pipe and csv files in the inputted directory
+        /// </summary>
+        /// <param name="directoryPath">Path to the file directory to be parsed</param>
+        public void ParseFiles(string directoryPath)
+        {
+            Console.WriteLine($"Processing files...{Environment.NewLine}");
+
+            List<string> files = GetFilesToProcess(directoryPath);
+
+            if (errors.Count == 0)
+            {
+                GenerateFiles(files);
+            }
+
+            if (errors.Count == 0)
+            {
+                //loop thorough files here and switch based on file extension
+                errors = engine.ProcessFiles(filesToProcess);
+            }
+
+            if (errors.Count > 0)
+            {
+                Console.WriteLine("File process unsuccessful!");
+                foreach (var error in errors)
+                {
+                    Console.WriteLine($"Error Message: {error.message}. Error Source: {error.source}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Files processed successfully!");
+            }
+        }
+
+        /// <summary>
+        /// Makes a list of all files in the inputted directory.
+        /// </summary>
+        /// <param name="directoryPath">Target directory path.</param>
+        /// <returns>A list of type string with all file paths in the input directory.</returns>
+        private List<string> GetFilesToProcess(string directoryPath)
+        {
+            List<string> files = new List<string>();
+
+            try
+            {
+                files = Directory.GetFiles(directoryPath).Where(x => !x.Contains("_out")).ToList();
+            }
+            catch (IOException ex)
+            {
+                errors.Add(new Error(ex.Message, "Parser.GetFilesToProcess()"));
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                errors.Add(new Error(ex.Message, "Parser.GetFilesToProcess()"));
+            }
+            catch (ArgumentException ex)
+            {
+                errors.Add(new Error(ex.Message, "Parser.GetFilesToProcess()"));
+            }
+            catch (Exception ex)
+            {
+                errors.Add(new Error(ex.Message, "Parser.GetFilesToProcess()"));
+            }
+
+            return files;
+        }
+
+        /// <summary>
+        /// From a list of file paths, generates objects of type MyFile and adds them to the FileeToProcess list.
+        /// </summary>
+        /// <param name="files">List of files paths to be turned into MyFile Objects</param>
+        private void GenerateFiles(List<string> files)
+        {
+            foreach (var file in files)
+            {
+                if (file.EndsWith(Constants.FileExtensions.PIPE))
+                {
+                    MyFile tempFile = new MyFile(file, Constants.FileDelimieters.PIPE, Constants.FileExtensions.PIPE);
+                    filesToProcess.Add(tempFile);
+                }
+                else if (file.EndsWith(Constants.FileExtensions.CSV))
+                {
+                    MyFile tempFile = new MyFile(file, Constants.FileDelimieters.CSV, Constants.FileExtensions.CSV);
+                    filesToProcess.Add(tempFile);
+                }
+                else
+                {
+                    errors.Add(new Error($"Invalid file extenstion processed: {file.Substring(file.LastIndexOf('.'))}", "Parser.GenerateFiles()"));
+                    break;
+                }
+            }
+        }
+    }
+}
