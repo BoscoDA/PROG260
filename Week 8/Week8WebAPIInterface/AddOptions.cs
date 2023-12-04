73652af2f4036be3f36d9c3db013a361b1b6e4e5
BoscoDA <nickwe02@gmail.com>
2023-12-04 11:18:56 -0600

    implement simple console app that interacts with the web server.

---
+++ Week 8/Week8WebAPIInterface/AddOptions.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week8WebAPIInterface
+{
+    [Verb("add", HelpText = "Adds a fruit to the database.")]
+    public class AddOptions
+    {
+        [Option('f', "fruit_name", Required = true, HelpText ="Name of fruit to be added to database.")]
+        public string Fruit_Name { get; set; }
+    }
+}
