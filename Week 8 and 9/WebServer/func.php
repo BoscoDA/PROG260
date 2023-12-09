19f4305ad9f382dc5bcf5fa6a84a0cba11d8fd06
BoscoDA <nickwe02@gmail.com>
2023-12-09 14:15:38 -0600

    add rest behavior and cron job

--- Week 8 and 9/WebServer/func.php
+++ Week 8 and 9/WebServer/func.php
@@ -1,16 +1,44 @@
 <?php
+date_default_timezone_set('Etc/UTC');
+class DailyFruit
+{
+    public $date;
+    public $total;
+
+    function set_date($date)
+    {
+        $this->date = $date;
+    }
+
+    function get_date()
+    {
+        return $this->date;
+    }
+
+    function set_total($total)
+    {
+        $this->total = $total;
+    }
+
+    function get_total()
+    {
+        return $this->total;
+    }
+}
 
 class Helper
 {
+
     public function AddFruit($fruit)
     {
         require 'conn.php';
 
         $query = "INSERT INTO fruit(name) VALUES('".$fruit."') ";
         $result = mysqli_query($link,$query);
-        echo $result;
 
         $link->close();
+
+        return $result;
     }
 
     public function RemoveFruit($fruit)
@@ -19,9 +47,10 @@ class Helper
         
         $query = "DELETE FROM fruit WHERE name = '".$fruit."' ";
         $result = mysqli_query($link,$query);
-        echo $result;
 
         $link->close();
+
+        return $result;
     }
 
     public function PrintFruit()
@@ -30,6 +59,9 @@ class Helper
         
         $query = "SELECT * FROM fruit";
         $result = mysqli_query($link,$query);
+
+        $link->close();
+
         if($result == 1)
         {
             if($result->num_rows>0)
@@ -44,11 +76,99 @@ class Helper
         }
         else
         {
-            echo $result;
+            return $result;
+        }
+    }
+
+    public function GetFruit()
+    {
+        require 'conn.php';
+        
+        $fruitArr = Array();
+
+        $query = "SELECT * FROM fruit";
+
+        if($result = mysqli_query($link,$query))
+        {
+            if($result->num_rows>0)
+            {
+                while($row = $result->fetch_array(MYSQLI_ASSOC))
+                {
+                    array_push($fruitArr, $row['name']);
+                }
+            }
+        }
+        else
+        {
+            array_push($fruitArr, 'FAIL');
+        }
+
+        $link->close();
+
+        return $fruitArr;
+    }
+
+    public function ProcessFruit()
+    {
+        //total pruchased fruit today
+        require 'conn.php';
+        
+        $fruitArr = Array();
+
+        $query = "SELECT * FROM fruit";
+
+        if($result = mysqli_query($link,$query))
+        {
+            if($result->num_rows>0)
+            {
+                while($row = $result->fetch_array(MYSQLI_ASSOC))
+                {
+                    array_push($fruitArr, $row['name']);
+                }
+            }
+
+            $count = sizeof($fruitArr);
+
+            $query = "INSERT INTO fruit_daily_result(total) VALUES('".$count."') ";
+            mysqli_query($link,$query);
         }
 
         $link->close();
     }
+
+    public function GetDaily()
+    {
+        require 'conn.php';
+        
+        $fruitArr = Array();
+
+        $query = "SELECT * FROM fruit_daily_result";
+
+        if($result = mysqli_query($link,$query))
+        {
+            if($result->num_rows>0)
+            {
+                while($row = $result->fetch_array(MYSQLI_ASSOC))
+                {
+                    $temp = new DailyFruit();
+                    $date = date('Y-m-d\TH:i:s', strtotime($row['date']));
+                    $temp->set_date($date);
+                    $temp->set_total($row['total']);
+                    array_push($fruitArr, $temp);
+                }
+            }
+        }
+        else
+        {
+            array_push($fruitArr, 'FAIL');
+        }
+
+        $link->close();
+
+        return $fruitArr;
+    }
 }
+
+
 
 ?>
--- Week 8 and 9/WebServer/index.php
+++ Week 8 and 9/WebServer/index.php
@@ -1,24 +1,97 @@
 <?php
-
     require "func.php";
     $helper = new Helper();
+    date_default_timezone_set('Etc/UTC');
+    $gmdate = date('Y-m-d\TH:i:s', time());
+    $returnValue = array("success" => false, "serverTime" => stripslashes($gmdate));
+
 
     if(isset($_REQUEST['add-fruit']))
     {
         $fruit = $_REQUEST['add-fruit'];
-        $helper->AddFruit($fruit);
+        $result = $helper->AddFruit($fruit);
+        $returnValue['success'] = $result;
+
+        echo json_encode($returnValue);
     }
 
-    if(isset($_REQUEST['remove-fruit']))
+    else if(isset($_REQUEST['remove-fruit']))
     {
         $fruit = $_REQUEST['remove-fruit'];
         $helper->RemoveFruit($fruit);
+        echo json_encode($returnValue);
     }
 
-    if(isset($_REQUEST['print-fruit']))
+    else if(isset($_REQUEST['print-fruit']))
     {
         $fruit = $_REQUEST['print-fruit'];
         $helper->PrintFruit($fruit);
+        echo json_encode($returnValue);
+    }
+
+    else if(isset($_REQUEST['process-fruit']))
+    {
+        $fruit = $_REQUEST['process-fruit'];
+        $helper->ProcessFruit($fruit);
+    }
+
+    else if(isset($_REQUEST['get-daily']))
+    {
+        $fruit = $_REQUEST['get-daily'];
+        $result = $helper->GetDaily($fruit);
+        
+        $returnValue['dailys'] = $result;
+        $returnValue['success'] = TRUE;
+
+        echo json_encode($returnValue);
+
+    }
+
+    else if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        if(isset($_POST['add-fruit']))
+        {
+            $fruit = $_POST['add-fruit'];
+            $result = $helper->AddFruit($fruit);
+            $returnValue['success'] = $result;
+            echo json_encode($returnValue);
+        }
+
+        if(isset($_POST['remove-fruit']))
+        {
+            $fruit = $_POST['remove-fruit'];
+            $result = $helper->RemoveFruit($fruit);
+            $returnValue['success'] = $result;
+            echo json_encode($returnValue);
+        }
+    }
+
+    else if($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+
+        $result = $helper->GetFruit();
+
+        if($result[0] == 'FAIL')
+        {
+            $returnValue['success'] = FALSE;
+            $returnValue['fruit'] = NULL;
+        }
+
+        else
+        {
+            $returnValue['fruit'] = $result;
+            $returnValue['success'] = true;
+        }
+
+        
+        echo json_encode($returnValue);
+    }
+
+    else{
+        $returnValue['success'] = FALSE;
+        $returnValue['fruit'] = NULL;
+
+        echo json_encode($returnValue);
     }
 
 ?>
